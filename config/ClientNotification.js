2b25e564d9e4cf13f1033b3cf6a52b27e868bad3
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-12-01 10:50:05 -0300

    #

--- config/ClientNotification.js
+++ config/ClientNotification.js
@@ -11,10 +11,13 @@ const ClientNotification = (function (application) {
       })
 
       let sql = `
-      select id, clientPhone, clientEmail from scheduling 
+      select scheduling.id, clientPhone, clientEmail user.companyName. user.companyEmail 
+      from scheduling, employee, user 
       where year(_datetime) = year(ADDDATE(CURDATE(), INTERVAL 1 DAY))
       and month(_datetime) = month(ADDDATE(CURDATE(), INTERVAL 1 DAY))
-      and day(_datetime) = day(ADDDATE(CURDATE(), INTERVAL 1 DAY))`
+      and day(_datetime) = day(ADDDATE(CURDATE(), INTERVAL 1 DAY))
+      and scheduling.employeeId = employee.id
+      and employee.userId = user.id`
 
       connection.connect()
 
--- keep/script.sql
+++ keep/script.sql
@@ -1,3 +1,7 @@
-alter table user
-add column companyEmail varchar(50) default null after companyName;
-
+select scheduling.id as schedulingId, scheduling.clientPhone, scheduling.clientEmail, user.companyName, user.companyEmail 
+from scheduling, employee, user 
+where year(_datetime) = year(ADDDATE(CURDATE(), INTERVAL 1 DAY))
+and month(_datetime) = month(ADDDATE(CURDATE(), INTERVAL 1 DAY))
+and day(_datetime) = day(ADDDATE(CURDATE(), INTERVAL 1 DAY))
+and scheduling.employeeId = employee.id
+and employee.userId = user.id
